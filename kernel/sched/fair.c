d5cffcb7b26e4b96168fe7f357009144c24f7184
Templar Dev <wilfridus.it@gmail.com>
2026-02-14 07:53:46 +0000

    sched/fair: Configure SCHED_BORE for balanced responsiveness
    - Adjust min granularity to 3.5ms for fair task distribution
    - Configure wakeup granularity to 4.5ms
    - Disable tunable scaling for consistent cross-platform behavior
    - Balance: responsiveness, efficiency, and thermal stability

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -77,8 +77,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
  * (CFS  default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
 #ifdef CONFIG_SCHED_BORE
-unsigned int sysctl_sched_min_granularity			= 3000000ULL;
-static unsigned int normalized_sysctl_sched_min_granularity	= 3000000ULL;
+unsigned int sysctl_sched_min_granularity			= 3500000ULL;
+static unsigned int normalized_sysctl_sched_min_granularity	= 3500000ULL;
 #else // CONFIG_SCHED_BORE
 unsigned int sysctl_sched_min_granularity			= 750000ULL;
 static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
@@ -107,8 +107,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * (CFS  default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
 #ifdef CONFIG_SCHED_BORE
-unsigned int sysctl_sched_wakeup_granularity			= 4000000UL;
-static unsigned int normalized_sysctl_sched_wakeup_granularity	= 4000000UL;
+unsigned int sysctl_sched_wakeup_granularity			= 4500000UL;
+static unsigned int normalized_sysctl_sched_wakeup_granularity	= 4500000UL;
 #else // CONFIG_SCHED_BORE
 unsigned int sysctl_sched_wakeup_granularity			= 1000000UL;
 static unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
@@ -120,11 +120,11 @@ const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 u8   __read_mostly sched_bore                   = 1;
 u8   __read_mostly sched_burst_exclude_kthreads = 1;
 u8   __read_mostly sched_burst_smoothness_long  = 1;
-u8   __read_mostly sched_burst_smoothness_short = 0;
+u8   __read_mostly sched_burst_smoothness_short = 1;
 u8   __read_mostly sched_burst_fork_atavistic   = 2;
 u8   __read_mostly sched_burst_penalty_offset   = 22;
-uint __read_mostly sched_burst_penalty_scale    = 1280;
-uint __read_mostly sched_burst_cache_lifetime   = 60000000;
+uint __read_mostly sched_burst_penalty_scale    = 960;
+uint __read_mostly sched_burst_cache_lifetime   = 80000000;
 #endif // CONFIG_SCHED_BORE
 
 int sched_thermal_decay_shift;
