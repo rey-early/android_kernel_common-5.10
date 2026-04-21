bb8b7fe42f42125a9cc6c242065dcbae55a5a8fb
rinnsakaguchi <ffrizz042@gmail.com>
2026-04-21 15:05:55 +0700

    Revert "timekeeping: Keep the tick alive when CPUs cycle out of s2idle"
    This reverts commit 5b90e3457a41aeea6e65164e5626a2710dfd7102.

--- kernel/time/tick-common.c
+++ kernel/time/tick-common.c
@@ -549,8 +549,6 @@ void tick_unfreeze(void)
 		touch_softlockup_watchdog();
 		tick_resume_local();
 	}
-	if (tick_do_timer_cpu == TICK_DO_TIMER_NONE)
-		tick_do_timer_cpu = cpu;
 
 	tick_freeze_depth--;
 
